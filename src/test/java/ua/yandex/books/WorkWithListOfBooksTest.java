885c5a651136b51b4e6701440e06ca227adc506b
Olha Tkachenko <oliatkachenko0@gmail.com>
2015-06-30 15:43:55 +0300

    Pizza1

--- src/test/java/ua/yandex/books/WorkWithListOfBooksTest.java
+++ src/test/java/ua/yandex/books/WorkWithListOfBooksTest.java
@@ -6,8 +6,6 @@
 package ua.yandex.books;
 
 import java.util.Map;
-import java.util.Collection;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
